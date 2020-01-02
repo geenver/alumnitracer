d92b1a6f370d22e47378788b3fc2889088e6381c
Geenver Batugon <batugongeenver@gmail.com>
2020-01-02 17:46:15 +0800

    registerForm

---
+++ components/registerForm.php
@@ -0,0 +1,12 @@
+<form>
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Name</label>
+        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Input your name">
+    </div>
+    <div class="form-group">
+        <label for="Email"> Email address </label>
+        <input type="email" class="form-control" id="email" placeholder="Email Address">
+        <button type="button" class="btn btn-primary mt-5">Submit</button>
+    </div>
+
+</form>
