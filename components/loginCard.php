97178bc6240a8a6e492c177cae41af75ba326b25
Geenver Batugon <batugongeenver@gmail.com>
2020-01-02 17:45:14 +0800

    logInCard

---
+++ components/loginCard.php
@@ -0,0 +1,8 @@
+<div class="card" style="width: 18rem;">
+    <img src="http://sample.em-n-em.us/wp-content/uploads/2019/07/slsu_text2.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Login</a>
+    </div>
+</div>
