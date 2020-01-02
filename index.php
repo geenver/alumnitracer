c3f612435a46f6b6c823b0796cdb970e6b60fb43
Geenver Batugon <batugongeenver@gmail.com>
2020-01-02 17:43:43 +0800

    header at footer
    Co-authored-by: Kris Carillo <kriscarilloxyz@gmail.com>

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('components/header.php'); ?>
+
+<body>
+	<?php include('components/navbar.php'); ?>
+	<div class="container">
+		<div class="row">
+			<?php include('components/registerForm.php'); ?>
+		</div>
+	</div>
+
+	<?php include('components/footer.php'); ?>
+</body>
+
+</html>
