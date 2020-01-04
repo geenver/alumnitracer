4cbf1f20b959da1cedb3d8540618bc4eb0c116d6
Geenver Batugon <batugongeenver@gmail.com>
2020-01-04 15:55:41 +0800

    putting google login in the alumni tracer

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <?php include('components/header.php'); ?>
 
 <body>
 	<?php include('components/navbar.php'); ?>
-	<div class="container">
-		<div class="row">
-			<?php include('components/registerForm.php'); ?>
-		</div>
-	</div>
-
+	<?php include('components/jumbotron.php'); ?>
 	<?php include('components/footer.php'); ?>
 </body>
 
